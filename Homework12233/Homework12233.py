580d94804114c0729e2c237787ba038cef9d468e
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-01-14 17:39:14 +0000

    Add project files.

---
+++ Homework12233/Homework12233.py
@@ -0,0 +1,6 @@
+num1 = int( input( "Pick a number "))
+num2 = int( input( "pick another number "))
+if num1 == num2 :
+    print( "the numbers are equal")
+else:
+    print( "the numbers are not equal")
